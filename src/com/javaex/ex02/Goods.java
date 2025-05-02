687915792a4baba817897c764d76173d5080fdc6
hyunjin-lee-2002 <hyunjinlee2002@gmail.com>
2025-05-02 16:53:07 +0900

    250502 practice5

--- src/com/javaex/ex02/Goods.java
+++ src/com/javaex/ex02/Goods.java
@@ -1,11 +1,38 @@
 package com.javaex.ex02;
 
 public class Goods {
-
 	
+	private String name;
+	private int price;
+	
+	public Goods(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	//edited
+	public Goods () {
+		
+	}
+	//
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public void showInfo() {
+		System.out.println("name: " + name + ", price: " + price);
+	}
 	
 }
-
-
-
-
